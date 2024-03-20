a9e34205595d86e6e1b64ca7b980adddf57a9a03
DiegoSou <diegomarcelosou@gmail.com>
2024-03-20 11:50:26 -0300

    fixing: using pytest.fixture decorator to create default instances in test class

--- pay/tests/test_payment.py
+++ pay/tests/test_payment.py
@@ -1,4 +1,4 @@
-from pytest import raises
+from pytest import raises, fixture
 from pay.payment import pay_order
 from pay.order import Order, LineItem
 from pay.credit_card import CreditCard
@@ -9,26 +9,24 @@ class PaymentProcessorMock:
         print(f"Charging {card} with amount ${amount/100:.2f}.")
 
 
-def test_pay_order_success():
-    order = Order()
-    order.line_items.append(LineItem('Test Line Item', 100, 5))
-    card = CreditCard(
+@fixture
+def card() -> CreditCard:
+    return CreditCard(
         card_number="1249190007575069",
         month=12,
         year=2025,
         amount=100,
     )
+
+
+def test_pay_order_success(card: CreditCard):
+    order = Order()
+    order.line_items.append(LineItem('Test Line Item', 100, 5))
     pay_order(order, card, PaymentProcessorMock())
 
 
-def test_pay_order_error():
+def test_pay_order_error(card: CreditCard):
     order = Order()
-    card = CreditCard(
-        card_number="1249190007575069",
-        month=12,
-        year=2025,
-        amount=100,
-    )
     with raises(ValueError) as error_info:
         pay_order(order, card, PaymentProcessorMock())
     assert "Can't pay an Order with total 0." in str(error_info.value)
--- pay/tests/test_processor.py
+++ pay/tests/test_processor.py
@@ -1,9 +1,20 @@
-from pytest import raises
+from pytest import raises, fixture
 from pay.credit_card import CreditCard
 from pay.processor import PaymentProcessor
 
 
 API_KEY = "6cfb67f3-6281-4031-b893-ea85db0dce20"
+
+
+@fixture
+def card() -> CreditCard:
+    return CreditCard(
+        card_number="1249190007575069",
+        month=12,
+        year=2025,
+        amount=100,
+    )
+
 
 def test_constructor():
     payment_processor = PaymentProcessor('')
@@ -38,66 +49,36 @@ def test_luhn_checksum_error():
     assert sum_is_not_valid
 
 
-def test_validate_card_success():
-    card = CreditCard(
-        card_number="1249190007575069",
-        month=12,
-        year=2025,
-        amount=100,
-    )
+def test_validate_card_success(card: CreditCard):
     payment_processor = PaymentProcessor(API_KEY)
     card_is_valid = payment_processor.validate_card(card)
     
     assert card_is_valid
 
 
-def test_validate_card_error():
-    card = CreditCard(
-        card_number="1249190007575069",
-        month=12,
-        year=1988,
-        amount=100,
-    )
+def test_validate_card_error(card: CreditCard):
+    card.year = 1988
     payment_processor = PaymentProcessor(API_KEY)
     card_is_not_valid = not payment_processor.validate_card(card)
     
     assert card_is_not_valid
 
 
-def test_charge_success():
-    credit_card = CreditCard(
-        card_number="1249190007575069",
-        month=12,
-        year=2025,
-        amount=100,
-    )
+def test_charge_success(card: CreditCard):
     payment_processor = PaymentProcessor(API_KEY)
-    payment_processor.charge(credit_card, 500)
+    payment_processor.charge(card, 500)
 
 
-def test_charge_card_error():
-    credit_card = CreditCard(
-        card_number="1249190007575069",
-        month=12,
-        year=1988,
-        amount=100,
-    )
+def test_charge_card_error(card: CreditCard):
+    card.year = 1988
     payment_processor = PaymentProcessor(API_KEY)
-    
     with raises(ValueError) as error_info:
-        payment_processor.charge(credit_card, 500)
+        payment_processor.charge(card, 500)
     assert "Invalid card" in str(error_info.value)
 
 
-def test_charge_api_error():
-    credit_card = CreditCard(
-        card_number="1249190007575069",
-        month=12,
-        year=1988,
-        amount=100,
-    )
+def test_charge_api_error(card: CreditCard):
     payment_processor = PaymentProcessor('invalid-key')
-    
     with raises(ValueError) as error_info:
-        payment_processor.charge(credit_card, 500)
+        payment_processor.charge(card, 500)
     assert "Invalid API key" in str(error_info.value)
